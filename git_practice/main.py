cacf65d5ced471a3603c4f8648c5bc28354f81c1
miaheirondelle <miroslava977@gmail.com>
2020-10-12 17:20:43 +0900

    Added logic to find traveler destinations and convert to internal data

--- git_practice/main.py
+++ git_practice/main.py
@@ -2,4 +2,17 @@ destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Pa
 
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
 
+def get_destination_index(destination):
+    destination_index = destinations.index(destination)
+    return destination_index
+
+#print(get_destination_index("Paris, France"))
+#traveler_destination = []
+def get_traveler_location(traveler):
+    traveler_destination = traveler[1]
+    traveler_destination_index = get_destination_index(traveler_destination)
+    return traveler_destination_index
+
+test_destination_index = get_traveler_location(test_traveler)
+print(test_destination_index)
 
