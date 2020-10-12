cb46c2018d72505bc909c175fc40e91137821cb5
miaheirondelle <miroslava977@gmail.com>
2020-10-13 01:06:36 +0900

    add

--- git_practice/main.py
+++ git_practice/main.py
@@ -40,3 +40,18 @@ add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
 add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
 add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
 add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+
+def find_attractions(destination, interests):
+    destination_index = get_destination_index(destination)
+    attractions_in_city = attractions[destination_index]
+    attractions_with_interest = []
+    for attraction in attractions_in_city:
+        possible_attraction = attraction
+        attraction_tags = attraction[1]
+        for interest in interests:
+            if interest in attraction_tags:
+                attractions_with_interest.append(possible_attraction[0])
+    return(attractions_with_interest)
+
+la_arts = find_attractions("Los Angeles, USA", ['art'])
+print(la_arts)
