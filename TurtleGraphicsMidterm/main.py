65fa72470f048bd9fa9115d05e636a559c188722
miaheirondelle <miroslava977@gmail.com>
2020-11-05 12:49:33 +0900

    Made a turtpe graphics piece using recursion with randomly selected colours

--- TurtleGraphicsMidterm/main.py
+++ TurtleGraphicsMidterm/main.py
@@ -1,11 +1,46 @@
-# Step 1: Make all the "turtle" commands available to us.
 import turtle
+import random
+painter = turtle.Turtle()
+turtle.bgcolor("black")
 
-# Step 2: Create a new turtle. We'll call it "bob"
-bob = turtle.Turtle()
+colors = ["blue", "white", "red", "yellow", "green", "orange"]
+def matryoshka(length):
+    painter.pencolor(colors[random.randint(0,5)])
+    painter.forward(length)
+    painter.left(60)
+    painter.forward(length)
+    painter.left(30)
+    painter.forward(length)
+    painter.left(45)
+    painter.forward(length)
+    painter.right(30)
+    painter.forward(length)
+    painter.left(75)
+    painter.forward(length)
+    painter.left(70)
+    painter.forward(length)
+    painter.right(25)
+    painter.forward(length)
+    painter.left(45)
+    painter.forward(length)
+    painter.left(30)
+    painter.forward(length)
+    painter.left(60)
+    painter.forward(length)
+    if length > 2:
+        matryoshka(length * 0.9)
 
-# Step 3: Move in the direction Bob's facing for 50 pixels
-bob.forward(50)
-
-# Step 4: We're done!
+turtle.speed(0)
+painter.hideturtle()
+painter.penup()
+painter.goto(0,-300)
+painter.showturtle()
+painter.pendown()
+matryoshka(165)
 turtle.done()
+
+
+
+
+
+
